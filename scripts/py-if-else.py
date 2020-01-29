117f38906e8296e1b446c5fe7f74f0c985842724
Kunal <kunaljha5@gmail.com>
2020-01-29 08:47:00 +0530

    py-if-else

---
+++ scripts/py-if-else.py
@@ -0,0 +1,34 @@
+#https://www.hackerrank.com/challenges/py-if-else/problem?h_r=next-challenge&h_v=zen
+#Task
+#Given an integer,n, perform the following conditional actions:
+
+#If n  is odd, print Weird
+#If n is even and in the inclusive range of 2 to 5 , print Not Weird
+#If n is even and in the inclusive range of 6 to 20 , print Weird
+#If n is even and greater than 20    , print Not Weird
+
+#Input Format
+
+#A single line containing a positive integer,
+#.
+#Constraints
+#Output Format
+#Print Weird if the number is weird; otherwise, print Not Weird.
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n%2 != 0 or n >= 6 and n <= 20:
+        print("Weird")
+    elif n >=2 and n <= 5 or n >= 20:
+        print("Not Weird")
+    else:
+        print("NA")
